58521d5a8cbf0e3bc9009509858aa7569a62eaeb
jcsamin099 <juliusceasarsamin@gmail.com>
2025-11-05 01:33:11 +0800

    FINAL COMMIT

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,6 +1,4 @@
 // src/pages/AdminDashboard.tsx
-import React from "react";
-import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 
